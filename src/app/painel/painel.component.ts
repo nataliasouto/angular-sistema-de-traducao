62443303b24f6041237fd8e198388bfd99efca1e
nataliasoutoo <nataliasoutoo@gmail.com>
2020-02-15 20:38:18 -0300

    criação de verificação de resposta

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -13,15 +13,37 @@ export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES
   public resposta: string
 
+  public rodada: number = 0
+  public rodadaFrase: Frase
 
-
-  constructor() { console.log(this.frases) } 
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase) } 
 
   ngOnInit() {
   }
 
-    atualizaResposta(resposta: Event):void{
+    public atualizaResposta(resposta: Event):void{
      this.resposta = ((<HTMLInputElement>resposta.target).value)
-      console.log(this.resposta)
+     // console.log(this.resposta)
+    }
+
+    public verificarResposta(): void {
+
+      if(this.rodadaFrase.frasePtBr == this.resposta) {
+        alert('A tradução está')
+      
+
+      //trocar pergunta
+
+      this.rodada++
+
+      // atualiza o objeto rodada frase
+      this.rodadaFrase = this.frases[this.rodada]
+
+    } else {
+      alert('A tradução está errada')
+    }
+      
     }
 }
